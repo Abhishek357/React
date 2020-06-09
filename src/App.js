09aff0a50b3f43655691ca91a5b01e1e337c7dec
Abhishek Singh Chauhan <abhisheksinghchauhan357@gmail.com>
2020-06-09 12:21:55 +0530

    app and menu component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React,{ Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent'
 
 // function App() {
 //   return (
@@ -14,12 +15,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
