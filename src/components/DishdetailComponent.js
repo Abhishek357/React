6943b3e839c1362244ea417250ba6c82588f7d40
Abhishek Singh Chauhan <abhisheksinghchauhan357@gmail.com>
2020-07-16 11:26:08 +0530

    fetch from server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors} from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl   } from '../shared/baseUrl';
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -121,7 +122,7 @@ class CommentForm extends Component {
       return(
           <div className="col-12 col-md-5 mt-1">
               <Card>
-                  <CardImg width="100%" object src={dish.image} alt={dish.name} />
+                  <CardImg width="100%" object src={baseUrl + dish.image} alt={dish.name} />
                   <CardBody>
                       <CardTitle>{dish.name}</CardTitle>
                       <CardText>{dish.description}</CardText>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -12,7 +12,7 @@ import About from './AboutUsComponent';
 // import { PROMOTIONS } from '../shared/promotion';
 import {Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { addComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 // function App() {
 //   return (
@@ -33,7 +33,9 @@ const mapStateToProps = state => {
 const mapDispatchToProps = (dispatch) => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
   fetchDishes: () => {dispatch(fetchDishes())},
-  resetFeedbackForm: () => { dispatch(actions.reset('feedback')) }
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback')) },
+  fetchComments: () => {dispatch(fetchComments())},
+  fetchPromos: () => {dispatch(fetchPromos())}
 });
 
 
@@ -57,6 +59,8 @@ class   Main extends Component {
 
   componentDidMount() {
     this.props.fetchDishes();
+    this.props.fetchPromos();
+    this.props.fetchComments();
   }
 
   render() {
@@ -65,7 +69,9 @@ class   Main extends Component {
         <Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]} 
         dishesLoading={this.props.dishes.isLoading}
         dishesErrMess={this.props.dishes.errMess}
-        promotion={this.props.promotions.filter((promo) => promo.featured)[0]} 
+        promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]} 
+        promosLoading={this.props.promotions.isLoading}
+        promosErrMess={this.props.promotions.errMess}
         leader={this.props.leaders.filter((leader) => leader.featured)[0]} 
         />
       );
@@ -76,7 +82,8 @@ class   Main extends Component {
         <DishdetailComponent dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
           isLoading={this.props.dishes.isLoading}
           errMess={this.props.dishes.errMess}          
-          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+          comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+          commentsErrMess={this.props.comments.errMess}          
           addComment={this.props.addComment}
         />
       );
